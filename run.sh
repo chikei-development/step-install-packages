bf1e9b1f0ad180aa6fdc83b010cf22222f93d51e
hotakasaito <teengenerate@gmail.com>
2016-01-26 17:24:59 +0900

    v0.0.14

--- run.sh
+++ run.sh
@@ -3,25 +3,16 @@ mkdir -p $APT_LIST_PATH
 sudo rm -fr /var/lib/apt/lists
 sudo ln -s $APT_LIST_PATH/ /var/lib/apt/lists
 
-#sudo sed -ie 's/httpredir.debian.org/ftp.debian.org/' /etc/apt/sources.list
-echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
-uname -a
-cat /etc/issue.net
-echo "----------------------------------------------------------------------------------------------------"
-cat <<EOF | sudo tee /etc/apt/sources.list
-deb http://ftp.jp.debian.org/debian/ jessie main
-#deb-src http://ftp.jp.debian.org/debian/ jessie main
-#deb http://security.debian.org/ jessie/updates main
-#deb-src http://security.debian.org/ jessie/updates main
-deb http://ftp.jp.debian.org/debian/ jessie-updates main
-#deb-src http://ftp.jp.debian.org/debian/ jessie-updates main
-EOF
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+if [ -n "$WERCKER_INSTALL_PACKAGES_DISABLE_SECURITY" ]
+then
+    sudo sed -iE "s/^(.+security\..+)$/#\1/i" /etc/apt/sources.list
+    sudo apt-get update -y
+fi
 
-sudo apt-get update -y
 if [ $( find $WERCKER_CACHE_DIR/wercker/aptupdated -mtime -1 | wc -l ) -eq 0 ]
 then
     touch $WERCKER_CACHE_DIR/wercker/aptupdated
+    sudo apt-get update -y
 fi
 
 sudo apt-get install $WERCKER_INSTALL_PACKAGES_PACKAGES -y --force-yes
