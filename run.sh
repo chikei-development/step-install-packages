90a9b64f9fc8d9a276c79a9fc03d98e409c2dcc8
hotakasaito <teengenerate@gmail.com>
2016-01-26 15:16:47 +0900

    overwrite /etc/apt/sources.list

--- run.sh
+++ run.sh
@@ -6,11 +6,11 @@ sudo ln -s $APT_LIST_PATH/ /var/lib/apt/lists
 #sudo sed -ie 's/httpredir.debian.org/ftp.debian.org/' /etc/apt/sources.list
 cat <<EOF | sudo tee /etc/apt/sources.list
 deb http://ftp.jp.debian.org/debian/ jessie main
-deb-src http://ftp.jp.debian.org/debian/ jessie main
+#deb-src http://ftp.jp.debian.org/debian/ jessie main
 deb http://security.debian.org/ jessie/updates main
-deb-src http://security.debian.org/ jessie/updates main
+#deb-src http://security.debian.org/ jessie/updates main
 deb http://ftp.jp.debian.org/debian/ jessie-updates main
-deb-src http://ftp.jp.debian.org/debian/ jessie-updates main
+#deb-src http://ftp.jp.debian.org/debian/ jessie-updates main
 EOF
 
 
