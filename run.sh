f4a4500c43cf760f28f6b69be82053a88383f5ff
hotakasaito <teengenerate@gmail.com>
2016-01-26 14:45:42 +0900

    overwrite /etc/apt/sources.list

--- run.sh
+++ run.sh
@@ -3,10 +3,9 @@ mkdir -p $APT_LIST_PATH
 sudo rm -fr /var/lib/apt/lists
 sudo ln -s $APT_LIST_PATH/ /var/lib/apt/lists
 
-cat /etc/apt/sources.list
-#sudo apt-get install software-properties-common -y --force-yes
-#sudo apt-get update -y
+sudo sed -ie 's/httpredir.debian.org/ftp.debian.org/' /etc/apt/sources.list
 
+sudo apt-get update -y
 if [ $( find $WERCKER_CACHE_DIR/wercker/aptupdated -mtime -1 | wc -l ) -eq 0 ]
 then
     touch $WERCKER_CACHE_DIR/wercker/aptupdated
