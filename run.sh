12c5bbe0c7f6efc57c0c165afba8ba7efea2b614
hotakasaito <teengenerate@gmail.com>
2016-06-20 12:25:55 +0900

    test apt-get options

--- run.sh
+++ run.sh
@@ -3,16 +3,18 @@ mkdir -p $APT_LIST_PATH
 sudo rm -fr /var/lib/apt/lists
 sudo ln -s $APT_LIST_PATH/ /var/lib/apt/lists
 
+export APT_OPTS='-o Acquire::Check-Valid-Until=false'
+
 if [ -n "$WERCKER_INSTALL_PACKAGES_DISABLE_SECURITY" ]
 then
     sudo sed -i -r "s/^(.+security\..+)$/#\1/i" /etc/apt/sources.list
-    sudo apt-get update -y
+    sudo apt-get $APT_OPTS update -y
 fi
 
 if [ $( find $WERCKER_CACHE_DIR/wercker/aptupdated -mtime -1 | wc -l ) -eq 0 ]
 then
     touch $WERCKER_CACHE_DIR/wercker/aptupdated
-    sudo apt-get update -y
+    sudo apt-get $APT_OPTS update -y
 fi
 
-sudo apt-get install $WERCKER_INSTALL_PACKAGES_PACKAGES -y --force-yes
+sudo apt-get $APT_OPTS install $WERCKER_INSTALL_PACKAGES_PACKAGES -y --force-yes
